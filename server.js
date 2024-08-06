edd72b179465264a6cc4b0cc200efb8b0eb9e24f
Ledexsoft <ledexsoft@gmail.com>
2024-08-06 17:55:41 -0400

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   analytics.html
    	new file:   desktop.ini
    	modified:   index.html
    	modified:   pages/dashboards/automotive.html
    	new file:   server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+// Definir las tres constantes
+const cadena1 = "Hola";
+const cadena2 = " ";
+const cadena3 = "Mundo";
+
+// Unir las tres cadenas en una sola variable global
+window.cadenaUnida = cadena1 + cadena2 + cadena3;
