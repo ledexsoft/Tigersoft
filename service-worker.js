a0d9447d25864431ee266859737f84c43027b978
Ledexsoft <ledexsoft@gmail.com>
2024-08-06 19:23:19 -0400

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	deleted:    assets/img/apple-icon.png
    	deleted:    assets/img/favicon.png
    	modified:   index.html
    	modified:   service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,59 @@
-//asignar un nombre y versión al caché
-const CACHE_NAME='v1.1_cache_tigersoft',
-urlsToCache= [
-    '/',
-    'index.html',
-    'assets/css/material-dashboard.min.css',
-    'assets/js/material-dashboard.min.js',
-    'assets/js/core/bootstrap.min.js',
-    'assets/js/core/popper.min.js',
-    'assets/img/ouytiger.webp',
-    'assets/img/photo-1611021061285-16c871740efa.avif',
-    'assets/img/IMG-20240806-WA0037.jpg',
-    'assets/img/ouytiger.webp'
-]
+// Asignar un nombre y versión al caché
+const CACHE_NAME = 'v1.1_cache_tigersoft';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'assets/css/material-dashboard.min.css',
+  'assets/js/material-dashboard.min.js',
+  'assets/js/core/bootstrap.min.js',
+  'assets/js/core/popper.min.js',
+  'assets/img/ouytiger.webp',
+  'assets/img/photo-1611021061285-16c871740efa.avif',
+  'assets/img/IMG-20240806-WA0037.jpg'
+];
 
 // Instalación del service worker
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then(function(cache) {
-          console.log('Cache abierta');
-          return cache.addAll(urlsToCache);
-        })
-    );
-  });
-  // Fetch del service worker
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request)
-        .then(function(response) {
-          if (response) {
-            return response;
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Cache abierta');
+        return cache.addAll(urlsToCache);
+      })
+      .catch(function(error) {
+        console.error('Error al abrir el cache:', error);
+      })
+  );
+});
+
+// Activación del service worker
+self.addEventListener('activate', function(event) {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
           }
-          return fetch(event.request);
         })
-    );
-  });
+      );
+    })
+  );
+});
+
+// Fetch del service worker
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+      .catch(function(error) {
+        console.error('Error en el fetch:', error);
+      })
+  );
+});
