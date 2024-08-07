f51aa03a1d8c4c04f67c78304af25fa04dbc3744
Ledexsoft <ledexsoft@gmail.com>
2024-08-07 00:14:52 -0400

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   analytics.html
    	deleted:    pages/ecommerce/products/edit-product-1.html
    	deleted:    pages/ecommerce/products/edit-product-2.html
    	modified:   pages/ecommerce/products/edit-product.html
    	deleted:    pages/ecommerce/products/new-product-1.html
    	deleted:    pages/ecommerce/products/new-product-2.html
    	modified:   pages/ecommerce/products/new-product.html
    	deleted:    pages/ecommerce/products/product-page-1.html
    	deleted:    pages/ecommerce/products/product-page-2.html
    	modified:   pages/ecommerce/products/product-page.html
    	deleted:    pages/ecommerce/products/products-list-1.html
    	deleted:    pages/ecommerce/products/products-list-2.html
    	modified:   pages/ecommerce/products/products-list.html
    	modified:   server.js

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
 // Definir las tres constantes
-const cadena1 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ";
-const cadena2 = "zdXBhYmFzZSIsInJlZiI6Imd0ZXdxZWR5em15bmV1ZGFtYW9jIiwicm9sZSI6InNlcnZpY";
-const cadena3 = "2Vfcm9sZSIsImlhdCI6MTcxMTg0MDIzNCwiZXhwIjoyMDI3NDE2MjM0fQ.73G9ftJfKNBzBaepT2DWlRCa11usXXbyZD-ZSFT24HM";
+const cadena1 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJzd3F";
+const cadena2 = "teG1hbnV0cWxheXpkdHBrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI4OTAyNTgsImV4cCI6";
+const cadena3 = "MjAzODQ2NjI1OH0.gqQlCMMzFEMGYRIMV1Yv8B_RbbXKb-69rlJU1tc1o6Y";
 
 // Unir las tres cadenas en una sola variable global
 window.cadenaUnida = cadena1 + cadena2 + cadena3;
 
 // Inicializa Supabase
 const supabase = createClient(
-    'https://gtewqedyzmyneudamaoc.supabase.co',
+    'https://bswqmxmanutqlayzdtpk.supabase.co',
     window.cadenaUnida
 );
 
 // Imprimir la cadena unida en la consola
 console.log(window.cadenaUnida);
+
+let { data: Inventario_de_Tienda, error } = await supabase
+  .from('Inventario_de_Tienda')
+  .select('*')
+          
+          
