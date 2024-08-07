a7a9f2bcd93904207ab77d4b509ba8123b67913b
Ledexsoft <ledexsoft@gmail.com>
2024-08-06 21:27:04 -0400

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   index.html
    	modified:   server.js

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+
 // Definir las tres constantes
-const cadena1 = "Hola";
-const cadena2 = " ";
-const cadena3 = "Mundo";
+const cadena1 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ";
+const cadena2 = "zdXBhYmFzZSIsInJlZiI6Imd0ZXdxZWR5em15bmV1ZGFtYW9jIiwicm9sZSI6InNlcnZpY";
+const cadena3 = "2Vfcm9sZSIsImlhdCI6MTcxMTg0MDIzNCwiZXhwIjoyMDI3NDE2MjM0fQ.73G9ftJfKNBzBaepT2DWlRCa11usXXbyZD-ZSFT24HM";
 
 // Unir las tres cadenas en una sola variable global
 window.cadenaUnida = cadena1 + cadena2 + cadena3;
+
+// Inicializa Supabase
+const supabase = createClient(
+    'https://gtewqedyzmyneudamaoc.supabase.co',
+    window.cadenaUnida
+);
+
+// Imprimir la cadena unida en la consola
+console.log(window.cadenaUnida);
